1ac1fb2bb37fa12a0468b08bb97fd0146e7d23c9
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2025-09-03 12:50:19 +0300

    fix(): fix paths

--- src/shared/paths.ts
+++ src/shared/paths.ts
@@ -1,13 +1,23 @@
-type PathLeaf<Args extends unknown[] = []> = {
+type ArgType = string | number | boolean;
+
+type PathLeaf<Args extends readonly ArgType[] = []> = {
   getHref: (...args: Args) => string;
 };
 
 type PathTree = {
-  [key: string]: PathTree | PathLeaf<unknown[]>;
-}
+  [key: string]: PathTree | PathLeaf<ArgType[]>;
+};
 
 export const paths = {
   home: {
-    getHref: () => `/` as string,
+    getHref: () => `/`,
+  },
+  blogs: {
+    root: {
+      getHref: () => `/blogs`,
+    },
+    posts: {
+      getHref: (slug: ArgType) => `/blogs/${slug}`,
+    },
   },
 } as const satisfies PathTree;
